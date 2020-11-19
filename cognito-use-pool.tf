78c2487868cc0146d03328bcb595a2599c451b14
Stefan Buys (Information Technology) <stefanbuys@capitecbank.co.za>
2020-11-19 15:31:07 +0200

    Added TF pool

--- cognito-use-pool.tf
+++ cognito-use-pool.tf
@@ -3,10 +3,10 @@ provider "aws" {
     region  = "eu-west-1"
 }
 
-# resource "aws_cognito_user_pool" "pool" {
-#     name = "tf-pool"
+resource "aws_cognito_user_pool" "pool" {
+    name = "tf-pool"
 
-#     lambda_config {
-#         pre_token_generation = "arn:aws:lambda:eu-west-1:970541181154:function:preTokenGenerationLambda"
-#     }
-# }
+    lambda_config {
+        pre_token_generation = "arn:aws:lambda:eu-west-1:970541181154:function:preTokenGenerationLambda"
+    }
+}
